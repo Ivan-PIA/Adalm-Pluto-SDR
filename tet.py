c7a1e9015e9fdad28b8c616f26e001a63c0a7354
Ivan-PIA <wind.vanya@gmail.com>
2023-09-13 18:55:48 +0700

    sin/cos plot

--- tet.py
+++ tet.py
@@ -1,27 +1,26 @@
-from mpl_toolkits.mplot3d import Axes3D
+import random
 import matplotlib.pyplot as plt
-import pandas as pd
- 
-# Скачиваем данные для графика.
-url = 'https://python-scripts.com/wp-content/uploads/2019/3d-data.csv'
-data = pd.read_csv(url)
- 
-# Преобразуем его в длинный формат
-df = data.unstack().reset_index()
-df.columns=["X", "Y", "Z"]
- 
-# Переименовываем старые названия столбцов в числовой формат.
-df['X'] = pd.Categorical(df['X'])
-df['X'] = df['X'].cat.codes
- 
-# Мы собираемся сделать 20 графиков, для 20 разных углов
-for angle in range(70, 210, 2):
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_trisurf(df['Y'], df['X'], df['Z'], cmap=plt.cm.viridis, linewidth=0.2)
- 
-    ax.view_init(30, angle)
- 
-    filename = 'frames/step'+str(angle)+'.png'
-    plt.savefig(filename, dpi=96)
-    plt.gca()
+
+import math
+f=1
+A=1
+p=1
+
+a = []
+tim = []
+c = []
+t=0
+
+for i in range(1000):
+    
+    c.append(A*math.cos(f*t+p))
+    a.append(A*math.sin(f*t+p))
+    t+=0.1
+    tim.append(t)
+
+plt.xlabel("time")
+plt.ylabel("Ampl")
+
+plt.plot(tim,a)
+plt.plot(tim,c)
+plt.show()
