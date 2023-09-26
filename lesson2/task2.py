b62390e9f1e345562fd60c901515c30622a8bacc
Ivan-PIA <wind.vanya@gmail.com>
2023-09-26 22:37:14 +0700

    task2 plot 3 in 1

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -1,30 +1,24 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import time
 
 
-#a = np.random.randint(1,50000)
-#b = np.random.randint(1,50000)
-data_time = []
-arr1 = []
 
-def time_sort_numpy():
-    #a = np.random.sample(5000000)
-    for i in range(10,5000000,200000):
-        
-        
-        a = np.random.sample(5000000)
-        start = time.time()
-        
-        np.sort(a)
 
-        end = time.time() - start
-        data_time.append(end)
-        arr1.append(i)
-        
-    return end
+n = 40
+# time vector
+t = np.linspace(0, 1, n)
+# sine wave
+x = np.sin(np.pi*t) + np.sin(2*np.pi*t) + np.sin(3*np.pi*t) + np.sin(5*np.pi*t)+1
 
-time_sort_numpy()
-plt.plot(arr1,data_time)
+fig = plt.figure(figsize=(16, 8), dpi=100)
+
+plt.plot(t, x, 'g', label = 'Аналоговый')
+plt.stem(t, x, 'y',label = 'Дискретный')
+plt.step(t, x,'r', label = 'Квантованный')
+
+plt.xlabel('Time, s')
+plt.ylabel('Amplitude')
+plt.grid(linewidth=0.5)
+plt.legend(fontsize=15)
 plt.show()
