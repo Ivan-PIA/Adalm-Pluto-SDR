5b9c955b74915bd5420fc86a7c8a6319217ad90b
Ivan-PIA <wind.vanya@gmail.com>
2023-10-23 13:00:07 +0700

    add comments

--- lesson6/test1.py
+++ lesson6/test1.py
@@ -15,22 +15,22 @@ sdr.rx_buffer_size = 10000
 sdr.tx_cyclic_buffer = False
 
 
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):                      # перевод из текста в биты
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):                    # перевод из битов в текст
     n = int(bits, 2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 
 
 
-t = text_to_bits("P")
+t = text_to_bits("q")
 str_modul = '1111111111'
 t=str_modul+t+'1'
 
 print(t)
-ampl = 2**9
+ampl = 2**14
 bit_1 = np.array([ampl+1j*ampl]*100)
 c = np.array([0]*1000)
 #f = np.concatenate([k,c])
@@ -39,28 +39,30 @@ bit_0 = np.array([ampl/10+ampl*1j/10]*100)
 
 
 
-def AM():
+def AM():                    # функция амплитудной модуляции если встречается "1" увеличиваем амплитуду и если "0" уменьшаем
     data = []
     for i in range(len(t)):
         if t[i]=='0':
             data = np.concatenate([data,bit_0])
         elif t[i]=='1':
             data = np.concatenate([data,bit_1])
-    return data    
+    return data
 
-data = AM()
+data = AM() # данные для передачи
 
 #plt.plot(data)
 
 d = []
 
-for r in range(100):
-    if (r==10 or r ==40 or r==80):
+for r in range(100):                #цикл для передачи данных и их принятие
+    if (r==10 or r ==40 or r==80): # в какое время передавать данные
         sdr.tx(data)
     rx = sdr.rx()
-    d = np.concatenate([d,abs(rx)])
+    d = np.concatenate([d,abs(rx)]) # слияние массивов
 str_after = '0000'
 print(d)
+
+
 for i in range(100):
     if d[i]>2800.0:
         str_after = str_after + '1'
