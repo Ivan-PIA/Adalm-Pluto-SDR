c2fd945cef78599479687c146e7142f1dd510f68
Ivan-PIA <wind.vanya@gmail.com>
2023-11-22 17:45:42 +0700

    debag save file

--- lesson10/test3.py
+++ lesson10/test3.py
@@ -5,20 +5,27 @@ import numpy as np
 import adi
 import time
 
-sdr = adi.Pluto("ip:192.168.3.1")
+sdr = adi.Pluto("ip:192.168.2.1")
+sdr.rx_lo = int(900e6)
+sdr.tx_lo = int(900e6)
 
+fs = 600e3
+Ft= 100e3
+ns = fs/Ft
+ts1 =np.array([0,0,1,0,0,1,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,1,0,1,1,1])
+b = np.ones(int(ns))
 def gen_sig():
     data=max_len_seq(6)[0]
 
     #Преобразование нулей и единиц последовательности в передаваемое сообщение 
     m=2*data-1
-    Ft= 100e3
-    fs = 600e3
+    #Ft= 100e3
+    #fs = 600e3
 
-    ns = fs/Ft
+    #ns = fs/Ft
     ts1 =np.array([0,0,1,0,0,1,0,1,1,1,0,0,0,0,1,0,0,0,1,0,0,1,0,1,1,1])
 
-    b = np.ones(int(ns)) #Коэффициенты фильтра интерполятора
+    #b = np.ones(int(ns)) #Коэффициенты фильтра интерполятора
 
     ts1t = 2*ts1-1
 
@@ -53,7 +60,7 @@ n_frame = len(data)
 
 sdr.tx(data)
 rx_data = sdr.rx()
-plt.plot(rx_data)
+#plt.plot(rx_data)
 plt.show()
 
 def listen_efir(data,n_frame):
@@ -73,4 +80,18 @@ def listen_efir(data,n_frame):
         plt.pause(0.05)
         time.sleep(0.1)
 
-np.savetxt (" my_data.csv", rx_data, delimiter=" , ")
+def rx_data_form(xrec):
+    
+    fsr=2*np.pi * rs/fs
+    b2,a2 = signal.butter(10,fsr)
+    y1 = signal.lfilter(b2,a2,xrec)
+    #y2=signal.decimate(y1,ns)
+    yf=np.convolve(np.abs(y1.real),b)
+    y2=signal.decimate(yf,ns)
+    y=np.correlate(y2, ts1)
+    plt.figure(2)
+    plt.plot(np.abs(y))
+    ind = np.argmax(abs(y),axis=0)
+    yy=y2[ind:ind+len(data)]
+    
+np.savetxt ("my_data2.csv", rx_data, delimiter=" , ")
