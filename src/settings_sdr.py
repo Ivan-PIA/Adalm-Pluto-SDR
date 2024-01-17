7f4da4af0c0ee16aaad1bcb800260f525502cd23
Ivan-PIA <wind.vanya@gmail.com>
2024-01-17 20:31:47 +0700

    add standart seting pluto

--- src/settings_sdr.py
+++ src/settings_sdr.py
@@ -2,21 +2,23 @@ import adi
 
 
 
-def standart_settings(Pluto_IP, sample_rate):
+def standart_settings(Pluto_IP = "192.168.2.1", sample_rate = 1e6, buffer_size = 1e3):
     sdr = adi.Pluto(Pluto_IP)
     sdr.sample_rate = sample_rate
+    sdr.buffer_size = buffer_size
+    sdr.b
     return sdr
 
 
-def init_rx(Pluto_IP, sample_rate, rx_lo, hardwaregain):
+def init_rx(Pluto_IP, sample_rate, rx_lo, gain_rx):
     sdr = standart_settings(Pluto_IP, sample_rate)
     sdr.rx_lo = rx_lo
-    sdr.rx_hardwaregain_chan0 = hardwaregain
+    sdr.rx_hardwaregain_chan0 = gain_rx
     
     
     
 
-def init_tx(Pluto_IP, sample_rate, rx_lo, hardwaregain):
+def init_tx(Pluto_IP, sample_rate, rx_lo, gain_tx):
     sdr = standart_settings(Pluto_IP, sample_rate)
     sdr.rx_lo = rx_lo
-    sdr.rx_hardwaregain_chan0 = hardwaregain
+    sdr.rx_hardwaregain_chan0 = gain_tx
