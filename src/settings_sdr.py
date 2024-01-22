2c799da78276b0b361eeef3df903258e2f369ca5
Ivan-PIA <wind.vanya@gmail.com>
2024-01-22 21:12:34 +0700

    change init_rx for two component

--- src/settings_sdr.py
+++ src/settings_sdr.py
@@ -6,19 +6,16 @@ def standart_settings(Pluto_IP = "192.168.2.1", sample_rate = 1e6, buffer_size =
     sdr = adi.Pluto(Pluto_IP)
     sdr.sample_rate = sample_rate
     sdr.buffer_size = buffer_size
-    sdr.b
     return sdr
 
 
-def init_rx(Pluto_IP, sample_rate, rx_lo, gain_rx):
-    sdr = standart_settings(Pluto_IP, sample_rate)
+def init_rx(sdr, rx_lo, gain_rx):
     sdr.rx_lo = rx_lo
     sdr.rx_hardwaregain_chan0 = gain_rx
     
     
     
 
-def init_tx(Pluto_IP, sample_rate, rx_lo, gain_tx):
-    sdr = standart_settings(Pluto_IP, sample_rate)
+def init_tx(sdr, rx_lo, gain_tx):
     sdr.rx_lo = rx_lo
     sdr.rx_hardwaregain_chan0 = gain_tx
