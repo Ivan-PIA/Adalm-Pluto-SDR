e81f7a6a050e6ba39ef6f5eb212c704ab6b60863
Ivan-PIA <wind.vanya@gmail.com>
2024-02-08 18:55:51 +0700

    eye_diagram

--- tests/test/test.py
+++ tests/test/test.py
@@ -1,16 +1,30 @@
 from context import QAM256, QAM64, QAM16, QPSK, randomDataGenerator, plot_QAM
 import matplotlib.pyplot as plt
+import numpy as np
 
-bit = randomDataGenerator(24000)
-QAM256 = QAM256(bit)
-QAM64 = QAM64(bit)
-QAM16 = QAM16(bit)
+bit = randomDataGenerator(1000)
+
+
+
+
+#plot_QAM(QPSK)
+
+#def eye_diagram(qpsk):
+
+def eye_diagram(qpsk):
+    #t = np.zeros(len(qpsk))
+    for i in range(len(qpsk)):
+        plt.plot(np.roll(qpsk[i:i+3],3))
+    plt.show()
+
+h1 = np.ones(10)
+
 QPSK = QPSK(bit)
+QPSK = np.repeat(QPSK, 10)
+conv = np.convolve(QPSK,h1)
+noise = np.random.normal(0,0.015,len(QPSK))
+QPSK= QPSK+noise
 
-plot_QAM(QPSK)
-
-plot_QAM(QAM16)
-
-plot_QAM(QAM64)
-
-plot_QAM(QAM256)
+eye_diagram(QPSK)
+#plt.plot(QPSK.real)
+#plt.show()
