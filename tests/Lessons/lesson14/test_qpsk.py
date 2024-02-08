c3455cd3e1da2ae8a64edeb5ba4d53680f8a19eb
Ivan-PIA <wind.vanya@gmail.com>
2024-02-08 18:48:46 +0700

    uncomment

--- tests/Lessons/lesson14/test_qpsk.py
+++ tests/Lessons/lesson14/test_qpsk.py
@@ -16,7 +16,7 @@ sdr.tx_lo = 2000000000
 sdr.tx_cyclic_buffer = True
 #sdr.tx_cyclic_buffer = False
 sdr.tx_hardwaregain_chan0 = -5
-sdr.gain_control_mode_chan0 = "fast_attack"
+sdr.gain_control_mode_chan0 = "manual"
 
 def sqrt_rc_imp(ns, alpha, m):
     n = np.arange(-m * ns, m * ns + 1)
@@ -73,82 +73,79 @@ xiq=2**14*x1
 n_frame= len(xiq)
 sdr.tx(xiq)
 
-while 1:
-    print(1)
-# =============================================================================
-# sdr.rx_rf_bandwidth = 1000000
-# sdr.rx_destroy_buffer()
-# sdr.rx_hardwaregain_chan0 = -5
-# sdr.rx_buffer_size =2*n_frame
-# xrec1=sdr.rx()
-# sdr.tx_destroy_buffer()
-# xrec = xrec1/np.mean(xrec1**2)
-# xrecf=np.convolve(xrec ,b)
-# plt.figure(2)
-# plt.scatter(xrec1.real,xrec1.imag)
-# plt.title('received signal before correction')
-# 
-# 
-# xf1=np.fft.fft(xrec**4,2048) # возводим в 4 степень отсчеты сигнала и вычисляем ДПФ
-# K=2*np.pi*np.linspace(-0.5,0.5,2048) # ось нормированных частот 
-# plt.figure(3)
-# plt.plot(K, np.abs(np.fft.fftshift(xf1)))
-# arg=np.argmax(np.fft.fftshift(xf1)) #находим индекс с максимальной амплитудой
-# offf=K[arg]/4 # грубое смещение частоты в рад/отсчет 
-# 
-# xrec_coarse=xrec*np.exp(-1j*offf) # коррекция 
-# plt.figure(4)
-# plt.scatter(xrec_coarse.real,xrec_coarse.imag)
-# plt.title('coarse fr.corr')
-# print(offf)
-# 
-# xrec_coarse_d=signal.decimate(xrec_coarse,ns)
-# iter=2*n_frame
-# 
-# iter=256
-# phi1 = np.zeros(iter) # фаза на выходе петли PLL для коррекции сигнала
-# s_loop =  np.zeros(iter, dtype = complex)
-# dds_mult =  np.zeros(iter, dtype = complex)
-# phi_error =  np.zeros(iter)
-# phi_error_filtered =  np.zeros(iter)
-# 
-# phi1[0]=offf
-# 
-# int_out = 0
-# Kp = 0.1 # коэффициент пропорциональности loop
-# Ki=0.1#оэффициент интегратора 
-# 
-# iter=256
-# for i in range(iter-1):
-#     s_loop[i]=np.exp(1j*phi1[i])
-#     dds_mult[i]=xrec_coarse_d[i]*np.conjugate(s_loop[i])
-#     #dds_mult[i]=xrec[i]*np.conjugate(s_loop[i])# 
-#     phi_error[i]=np.imag(dds_mult[i]) # фазовая ошибка 
-#     #phi_error[i]=np.imag(xrec[i])*np.real(s_loop[i])-np.real(xrec[i])*np.imag(s_loop[i])
-#     #phi_error[i]=np.angle(dds_mult[i])
-#     prop_out = phi_error[i]*Kp
-#     int_out = phi_error[i]*Ki+int_out
-#     phi_error_filtered[i]=prop_out+int_out
-#     phi1[i+1]=phi1[i]+phi_error_filtered[i]
-#     
-# #xrecc=xrec_coarse_d*np.exp(-1j*phi1)
-# xrecc=xrec_coarse_d*s_loop
-# plt.figure(5)
-# plt.scatter(xrecc.real,xrecc.imag)
-# plt.title('DPLL')
-# 
-# plt.figure(6)
-# plt.plot(phi_error_filtered)   
-#  
-# plt.figure(7)
-# plt.plot(np.arange(0,120),np.real(xrec[0:120]),np.arange(0,120),np.real(xrec_coarse[0:120]))
-#     
-# plt.figure(8)    
-# plt.plot(np.arange(0,120),np.real(xrec_coarse_d[0:120]),np.arange(0,120),np.real(xrecc[0:120]))    
-# 
-# plt.show()
-# 
-# =============================================================================
+sdr.rx_rf_bandwidth = 1000000
+sdr.rx_destroy_buffer()
+sdr.rx_hardwaregain_chan0 = -5
+sdr.rx_buffer_size =2*n_frame
+xrec1=sdr.rx()
+sdr.tx_destroy_buffer()
+xrec = xrec1/np.mean(xrec1**2)
+xrecf=np.convolve(xrec ,b)
+plt.figure(2)
+plt.scatter(xrec1.real,xrec1.imag)
+plt.title('received signal before correction')
+
+
+xf1=np.fft.fft(xrec**4,2048) # возводим в 4 степень отсчеты сигнала и вычисляем ДПФ
+K=2*np.pi*np.linspace(-0.5,0.5,2048) # ось нормированных частот 
+plt.figure(3)
+plt.plot(K, np.abs(np.fft.fftshift(xf1)))
+arg=np.argmax(np.fft.fftshift(xf1)) #находим индекс с максимальной амплитудой
+offf=K[arg]/4 # грубое смещение частоты в рад/отсчет 
+
+xrec_coarse=xrec*np.exp(-1j*offf) # коррекция 
+plt.figure(4)
+plt.scatter(xrec_coarse.real,xrec_coarse.imag)
+plt.title('coarse fr.corr')
+print(offf)
+
+xrec_coarse_d=signal.decimate(xrec_coarse,ns)
+iter=2*n_frame
+
+iter=256
+phi1 = np.zeros(iter) # фаза на выходе петли PLL для коррекции сигнала
+s_loop =  np.zeros(iter, dtype = complex)
+dds_mult =  np.zeros(iter, dtype = complex)
+phi_error =  np.zeros(iter)
+phi_error_filtered =  np.zeros(iter)
+
+phi1[0]=offf
+
+int_out = 0
+Kp = 0.1 # коэффициент пропорциональности loop
+Ki=0.1#оэффициент интегратора 
+
+iter=256
+for i in range(iter-1):
+    s_loop[i]=np.exp(1j*phi1[i])
+    dds_mult[i]=xrec_coarse_d[i]*np.conjugate(s_loop[i])
+    #dds_mult[i]=xrec[i]*np.conjugate(s_loop[i])# 
+    phi_error[i]=np.imag(dds_mult[i]) # фазовая ошибка 
+    #phi_error[i]=np.imag(xrec[i])*np.real(s_loop[i])-np.real(xrec[i])*np.imag(s_loop[i])
+    #phi_error[i]=np.angle(dds_mult[i])
+    prop_out = phi_error[i]*Kp
+    int_out = phi_error[i]*Ki+int_out
+    phi_error_filtered[i]=prop_out+int_out
+    phi1[i+1]=phi1[i]+phi_error_filtered[i]
+    
+#xrecc=xrec_coarse_d*np.exp(-1j*phi1)
+xrecc=xrec_coarse_d*s_loop
+plt.figure(5)
+plt.scatter(xrecc.real,xrecc.imag)
+plt.title('DPLL')
+
+plt.figure(6)
+plt.plot(phi_error_filtered)   
+ 
+plt.figure(7)
+plt.plot(np.arange(0,120),np.real(xrec[0:120]),np.arange(0,120),np.real(xrec_coarse[0:120]))
+    
+plt.figure(8)    
+plt.plot(np.arange(0,120),np.real(xrec_coarse_d[0:120]),np.arange(0,120),np.real(xrecc[0:120]))    
+
+plt.show()
+
+
 
 
 
