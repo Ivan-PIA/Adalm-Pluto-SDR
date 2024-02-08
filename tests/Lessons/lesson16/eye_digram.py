71708a245455c66fda9a955cfe090d4b7e90ce6b
Ivan-PIA <wind.vanya@gmail.com>
2024-02-09 00:00:29 +0700

    func eye_diagram() good

--- tests/Lessons/lesson16/eye_digram.py
+++ tests/Lessons/lesson16/eye_digram.py
@@ -9,19 +9,17 @@ bit = randomDataGenerator(100)
 
 #plot_QAM(QPSK)
 
-#def eye_diagram(qpsk):
 
 def eye_diagram(qpsk):
-    #t = np.zeros(len(qpsk))
     q = qpsk
-    for i in range(len(qpsk)):
-        #print(len(qpsk))
-        #print(np.roll(qpsk[i*10:i*10+10].real,-10))
+    print(qpsk.real)
+    for i in range(0,len(qpsk),20):
         
-        plt.plot(q[0:3].real)
-        q = np.roll(qpsk,-3)
+        plt.plot(q.real[0:20])
+        q = np.roll(qpsk,-20)
         qpsk = q
     plt.show()
+
 
 def eye_diagram1(qpsk):
     #t = np.zeros(len(qpsk))
@@ -39,14 +37,15 @@ def eye_diagram_var2(qpsk):
 
 
 
-#h1 = np.ones(10)
-
+h1 = np.ones(10)
 QPSK = QPSK(bit)
-#QPSK = np.repeat(QPSK, 10)
-#conv = np.convolve(QPSK,h1)
-noise = np.random.normal(0,0.015,len(QPSK))
-QPSK= QPSK+noise
+QPSK = np.repeat(QPSK, 10)
+conv = np.convolve(h1,QPSK)
+noise = np.random.normal(0,0.5,len(conv))
+noise_qpsk = np.random.normal(0,0.1,len(QPSK))
+conv= conv+noise
+QPSK = QPSK + noise_qpsk
 
-eye_diagram(QPSK)
+eye_diagram(conv)
 #plt.plot(QPSK.real)
 #plt.show()
