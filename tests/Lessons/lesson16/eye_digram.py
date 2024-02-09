867aec56a7e7a0748cd4a1785bde51da70a50613
Ivan-PIA <wind.vanya@gmail.com>
2024-02-10 01:28:19 +0700

    del unworking func, add func on_eye_dig

--- tests/Lessons/lesson16/eye_digram.py
+++ tests/Lessons/lesson16/eye_digram.py
@@ -2,10 +2,7 @@ from context import QAM256, QAM64, QAM16, QPSK, randomDataGenerator, plot_QAM
 import matplotlib.pyplot as plt
 import numpy as np
 
-bit = randomDataGenerator(100)
-
-
-
+bit = randomDataGenerator(1000)
 
 #plot_QAM(QPSK)
 
@@ -13,39 +10,38 @@ bit = randomDataGenerator(100)
 def eye_diagram(qpsk):
     q = qpsk
     print(qpsk.real)
-    for i in range(0,len(qpsk),20):
-        
-        plt.plot(q.real[0:20])
-        q = np.roll(qpsk,-20)
+    for i in range(0,len(qpsk),10):
+        plt.plot(q.real[0:10])
+        q = np.roll(qpsk,-10)
         qpsk = q
-    plt.show()
+    
 
-
-def eye_diagram1(qpsk):
-    #t = np.zeros(len(qpsk))
-    for i in range(len(qpsk)):
-        #print(len(qpsk))
-        #print(np.roll(qpsk.real,-10))
-        plt.plot(np.roll(qpsk.real[i:i+3],3))
-    plt.show()
-
-def eye_diagram_var2(qpsk):
-    qpsk = np.reshape(qpsk,10)
-    plt.plot(qpsk)
-    plt.show()
-
-
+def On_eye_dig(qpsk, index): # вычленяет семпл выбранный глазковой диаграммой 
+    decode_symbol = []
+    for i in range(index,len(qpsk),10):
+        #print()
+        decode_symbol.append(qpsk[i])
+    return np.asarray(decode_symbol)
 
 
 h1 = np.ones(10)
 QPSK = QPSK(bit)
 QPSK = np.repeat(QPSK, 10)
-conv = np.convolve(h1,QPSK)
-noise = np.random.normal(0,0.5,len(conv))
-noise_qpsk = np.random.normal(0,0.1,len(QPSK))
+conv = np.convolve(h1,QPSK,'same')
+noise = np.random.normal(0,0.1,len(conv))
 conv= conv+noise
-QPSK = QPSK + noise_qpsk
+#QPSK = QPSK + noise_qpsk
+#noise_qpsk = np.random.normal(0,0.1,len(QPSK))
+
+plt.figure(1)
+plt.scatter(QPSK.real, QPSK.imag)
+plt.figure(2)
 
 eye_diagram(conv)
-#plt.plot(QPSK.real)
-#plt.show()
+plt.figure(3)
+
+plt.figure(4)
+decode = On_eye_dig(conv,5)
+plt.scatter(decode.real, decode.imag)
+
+plt.show()
