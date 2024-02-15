f1313a65db61f21ba06aab3146fe120857663320
Ivan-PIA <wind.vanya@gmail.com>
2024-02-15 13:02:09 +0700

    add PPL and TED need test

--- tests/Lessons/lesson16/eye_digram.py
+++ tests/Lessons/lesson16/eye_digram.py
@@ -23,31 +23,69 @@ def On_eye_dig(qpsk, index): # вычленяет семпл выбранный 
         decode_symbol.append(qpsk[i])
     return np.asarray(decode_symbol)
 
+def PLL(conv):
+    mu = 0  # коэфф фильтра 
+    theta = 1 # начальная фаза
+    phase_error = np.zeros(len(conv))  # фазовая ошибка
+    output_signal = np.zeros(len(conv), dtype=np.complex128)
+
+    for n in range(len(conv)):
+        theta_hat = np.angle(conv[n])  # оценка фазы
+        #print(theta_hat)
+        phase_error[n] = theta_hat - theta  # фазовая ошибка
+        output_signal[n] = conv[n] * np.exp(-1j * theta)  # выходной сигнал
+        theta = theta + mu * phase_error[n]  # обновление
+    return output_signal
+
+def gardner_te_detector(symbol_seq, samples_per_symbol):
+    te = np.zeros(len(symbol_seq), dtype=np.complex128)
+    
+    for i in range(len(symbol_seq)):
+        if i < 2*samples_per_symbol:
+           continue
+        
+        te[i] = symbol_seq[i-samples_per_symbol] * (symbol_seq[i] - symbol_seq[i-2*samples_per_symbol])
+        
+    return te
 
 h1 = np.ones(10)
-
 
 QPSK = QPSK(bit)
 QPSK = np.repeat(QPSK, 10)
 noise = np.random.normal(0,0.2,len(QPSK))
-QPSK = QPSK +noise
+QPSK = QPSK + noise
 
 conv = np.convolve(h1,QPSK,'full')
+
 
 #conv= conv+
 #QPSK = QPSK + noise_qpsk
 #noise_qpsk = np.random.normal(0,0.1,len(QPSK))
 
-plt.figure(1)
-plt.scatter(QPSK.real, QPSK.imag)
-plt.figure(2)
 
-eye_diagram(conv)
+#plt.figure(1)
+#plt.scatter(QPSK.real, QPSK.imag)
 
-plt.figure(3)
-plt.plot(conv.real)
-plt.figure(4)
-decode = On_eye_dig(conv,9)
-plt.scatter(decode.real, decode.imag)
+#plt.figure(2)
+#eye_diagram(conv)
 
+#plt.figure(3)
+#plt.plot(conv.real)
+
+#plt.figure(4)
+conv = On_eye_dig(conv,9)
+#plt.scatter(conv.real, conv.imag)
+
+
+output_signal = PLL(conv)
+plt.figure(5)
+plt.scatter(output_signal.real,output_signal.imag)
+
+
+#plt.figure(6)
+#plt.plot(phase_error)
+te = gardner_te_detector(conv, 10)
+plt.figure(6)
+plt.scatter(te.real, te.imag)
+print(te)
 plt.show()
