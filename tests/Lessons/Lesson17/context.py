846ce3fe16ca9a891bf9e0ecfe27525ade0a087f
Ivan-PIA <wind.vanya@gmail.com>
2024-02-19 13:24:48 +0700

    context

---
+++ tests/Lessons/Lesson17/context.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+from src.hell import printt
+#from src.settings_sdr import *
+from src.plots import plot_QAM
+from src.modular import *
+from src.base import *
+
