b4cb355bb06c89a3867ecb6fc0cb9ad84bdb8ce5
Ivan-PIA <wind.vanya@gmail.com>
2024-02-27 16:48:23 +0700

    in text_to_bit return np.array [1,0,1,1 ....]

--- src/base.py
+++ src/base.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 def randomDataGenerator(size):
 	data = [random.randint(0, 1) for i in range(size)]
@@ -6,7 +7,7 @@ def randomDataGenerator(size):
 
 def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
+    return np.asarray(list(map(int,bits.zfill(8 * ((len(bits) + 7) // 8)))))
 
 def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits, 2)
