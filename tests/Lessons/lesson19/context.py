22871142570baee629c34aa1c31cefec9acfe927
Ivan-PIA <wind.vanya@gmail.com>
2024-03-20 10:45:08 +0700

    add context

---
+++ tests/Lessons/lesson19/context.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+from src.hell import printt
+from src.settings_sdr import *
+from src.plots import plot_QAM
+from src.modular import *
+from src.demodular import *
+from src.base import *
+
