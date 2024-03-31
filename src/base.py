6ad9b50ee18f8a2af564739f01da9bca3a554bbd
Ivan-PIA <wind.vanya@gmail.com>
2024-04-01 04:37:56 +0700

    add norlalize correlation, for synchro_symbol

--- src/base.py
+++ src/base.py
@@ -9,9 +9,19 @@ def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
     return np.asarray(list(map(int,bits.zfill(8 * ((len(bits) + 7) // 8)))))
 
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
-    bits = list(bits)
-    bits = list(map(int,bits))
-    bits = "".join(map(str, bits))
-    n = int(bits, 2)
-    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+
+def bits_array_to_text(bits_array):
+    bits_string = ''.join([str(bit) for bit in bits_array])
+    bits_string = bits_string.replace(" ", "")
+    n = int(bits_string, 2)
+    text = n.to_bytes((n.bit_length() + 7) // 8, 'big').decode('latin1')
+    return text
+
+def norm_corr(x,y):
+    #x_normalized = (cp1 - np.mean(cp1)) / np.std(cp1)
+    #y_normalized = (cp2 - np.mean(cp2)) / np.std(cp2)
+
+    c_real = np.vdot(x.real, y.real) / (np.linalg.norm(x.real) * np.linalg.norm(y.real))
+    c_imag = np.vdot(x.imag, y.imag) / (np.linalg.norm(x.imag) * np.linalg.norm(y.imag))
+    
+    return c_real+1j*c_imag
