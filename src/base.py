74174ea454ed642e71cc70b703de519c5dc1e364
Ivan-PIA <wind.vanya@gmail.com>
2024-04-22 09:07:30 +0700

    add zadoff_chu

--- src/base.py
+++ src/base.py
@@ -25,3 +25,26 @@ def norm_corr(x,y):
     c_imag = np.vdot(x.imag, y.imag) / (np.linalg.norm(x.imag) * np.linalg.norm(y.imag))
     
     return c_real+1j*c_imag
+
+
+def zadoff_chu(N=1, u=25, PSS=False):
+    """
+    Zadoff-Chu sequence
+        N - length
+        
+        u - root index 25 29 34
+        
+        PSS [optional] - Primary synchronization signal
+            N - 63 
+            Len - 62
+    """
+    if PSS:
+        N = 63
+        n = np.arange(0, 31)
+        ex1 = np.exp(-1j * np.pi * u * n * (n + 1) / N)
+        n = np.arange(31, 62)
+        ex2 = np.exp(-1j * np.pi * u * (n + 1) * (n + 2) / N)
+        return np.concatenate([ex1, ex2])
+    else:  
+        n = np.arange(0, N)
+        return np.exp(-1j * np.pi * u * n * (n + 1) / N)
