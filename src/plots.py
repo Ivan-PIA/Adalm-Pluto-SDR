6f0ef3a78d1edef13d20ea453776fade90ced3ca
Ivan-PIA <wind.vanya@gmail.com>
2024-04-22 09:06:47 +0700

    add resource_grid

--- src/plots.py
+++ src/plots.py
@@ -6,9 +6,10 @@
     - на вход подается numpy array комплексных чисел
 '''
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_QAM(a, title = "qam"):
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(6,6))
     plt.title(title)
     plt.grid(0)
     plt.axhline(y=0,color = 'red')
@@ -21,5 +22,32 @@ def plot_QAM(a, title = "qam"):
 
 
 
-def drav_plot():
-    pass
+def resource_grid(data, count_frames, len_frame, Nfft, cp ): 
+    """
+        data - данные для ресурсной сетки 
+        count_frame - количество OFDM фреймов бля ресурсной сетки
+        len_frame - количество ofdm-символов в ofdm фрейме
+        Nftt - количество поднесущих
+        cp - защитный префикс
+
+    """
+    data1 = data[:(Nfft+cp)*len_frame*count_frames]
+    half_nfft = Nfft//2
+
+    # преобразуем в матрицу 
+    data_freq = data1.reshape(len_frame*count_frames, (Nfft+cp))
+
+    # обрезаем циклический префикс
+    data1 = data_freq[:, cp:]
+    # производим обратное преобразование фурье и транспонируем матрицу для удобного вывода на карте
+    data2 = np.fft.fft(data1).T
+
+    # переставляем строки местами из-за не шифтнутых частот
+    temp = np.copy(data2[0:half_nfft, :])
+    data2[0:half_nfft, :] = data2[half_nfft:Nfft, :]
+    data2[half_nfft:Nfft, :] = temp
+
+    plt.figure()
+    plt.imshow(abs(data2), cmap='jet',interpolation='nearest', aspect='auto')
+    plt.colorbar()
+    plt.show()
