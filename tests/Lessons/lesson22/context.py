5c70e892d34bafd8c1a277dcf144ee71ed60246c
Ivan-PIA <wind.vanya@gmail.com>
2024-05-21 01:11:44 +0700

    add contex

---
+++ tests/Lessons/lesson22/context.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+
+from src.settings_sdr import *
+from src.plots import *
+from src.modular import *
+from src.demodular import *
+from src.base import *
+
